3913929b30b432af0810fa1516faec7c69d12a18
youngjun.son <syam2000@naver.com>
2020-02-17 21:25:15 +0900

    sparksql-count

--- src/main/scala/sparkcollection/aggregation/AggregationMain.scala
+++ src/main/scala/sparkcollection/aggregation/AggregationMain.scala
@@ -1,7 +1,8 @@
 package sparkcollection.aggregation
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{SparkSession, TypedColumn}
+import org.apache.spark.{SparkConf}
+import org.apache.spark.sql.functions._
 
 /**
  * @author 손영준 (youngjun.son@navercorp.com)
@@ -19,5 +20,11 @@ object AggregationMain {
 
     val statesDF = spark.read.option("header", "true").option("inferschema", "true").option("sep", ",").csv(inputPath)
     statesDF.show(5)
+
+    statesDF.select(col("*")).agg(count("State")).show
+    statesDF.select(count("State")).show
+
+    statesDF.select(col("*")).agg(countDistinct("State")).show
+    statesDF.select(countDistinct("State")).show
   }
 }
