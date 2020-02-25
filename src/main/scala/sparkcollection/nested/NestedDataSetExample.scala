935c060ae18400e2eff7790eeda4efd8e3961eca
youngjun.son <syam2000@naver.com>
2020-02-26 03:36:43 +0900

    spark-json

---
+++ src/main/scala/sparkcollection/nested/NestedDataSetExample.scala
@@ -0,0 +1,46 @@
+package sparkcollection.nested
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+/**
+ * @author 손영준 (youngjun.son@navercorp.com)
+ */
+object NestedDataSetExample {
+  def main(args: Array[String]): Unit = {
+    val jsonPath = args(0)
+
+    val conf = new SparkConf()
+    val spark = SparkSession
+      .builder()
+      .appName("NestedDataSet")
+      .config(conf)
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val jsonDf = spark.read.json(jsonPath)
+    jsonDf.foreach(row => {
+      val group = row.getAs("group")
+      println("row : " + row + ", group : "+group)
+    })
+
+    val dataset = spark.read.json(jsonPath).as[GroupBean]
+    dataset.foreach(row => {
+      val group = row.group
+      val time = row.time
+      println("dataframe : " + row + ", group : " + group + ", time : " + time)
+    })
+
+    dataset.printSchema()
+
+    dataset.toDF().createOrReplaceTempView("case_table")
+    spark.sql("select * from case_table").rdd.foreach(println)
+
+    spark.stop()
+  }
+
+  case class GroupBean(group: String, time: Long, value: Long, nested: Array[NestedBean])
+
+  case class NestedBean(col1: Long, col2: Long)
+}
