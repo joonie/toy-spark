a67a941d52d3135d2f481c3d56be0ff4f04a6565
youngjun.son <syam2000@naver.com>
2020-03-07 18:33:56 +0900

    spark-json-load

--- src/main/scala/sparkcollection/nested/NestedDataSetExample.scala
+++ src/main/scala/sparkcollection/nested/NestedDataSetExample.scala
@@ -22,28 +22,53 @@ object NestedDataSetExample {
 
     import spark.implicits._
 
+    /**
+     * row : [A,WrappedArray([0.1,0.2], [1.1,1.2]),5,3], group : A
+     * row : [B,WrappedArray([1.0,2.0], [1.0,2.0]),5,3], group : B
+     * row : [C,WrappedArray([1.0,2.0], [1.0,2.0]),5,3], group : C
+     */
     val jsonDf = spark.read.json(jsonPath)
-    jsonDf.foreach(row => {
+    jsonDf.collect().foreach(row => {
       val group = row.getAs[String]("group")
       println("row : " + row + ", group : "+group)
     })
 
+    /**
+     * dataframe : GroupBean(A,5.0,3.0,[L$line45.$read$$iw$$iw$NestedBean;@4274af83), group : A, time : 5.0
+     * dataframe : GroupBean(B,5.0,3.0,[L$line45.$read$$iw$$iw$NestedBean;@443ed4f0), group : B, time : 5.0
+     * dataframe : GroupBean(C,5.0,3.0,[L$line45.$read$$iw$$iw$NestedBean;@6383b7da), group : C, time : 5.0
+     */
     val dataset = spark.read.json(jsonPath).as[GroupBean]
-    dataset.foreach(row => {
+    dataset.collect().foreach(row => {
       val group = row.group
       val time = row.time
       println("dataframe : " + row + ", group : " + group + ", time : " + time)
     })
 
+    /**
+     * root
+     * |-- group: string (nullable = true)
+     * |-- nested: array (nullable = true)
+     * |    |-- element: struct (containsNull = true)
+     * |    |    |-- col1: double (nullable = true)
+     * |    |    |-- col2: double (nullable = true)
+     * |-- time: long (nullable = true)
+     * |-- value: long (nullable = true)
+     */
     dataset.printSchema()
 
+    /**
+     * [A,WrappedArray([0.1,0.2], [1.1,1.2]),5,3]
+     * [B,WrappedArray([1.0,2.0], [1.0,2.0]),5,3]
+     * [C,WrappedArray([1.0,2.0], [1.0,2.0]),5,3]
+     */
     dataset.toDF().createOrReplaceTempView("case_table")
-    spark.sql("select * from case_table").rdd.foreach(println)
+    spark.sql("select * from case_table").rdd.collect().foreach(println)
 
     spark.stop()
   }
 
-  case class GroupBean(group: String, time: Long, value: Long, nested: Array[NestedBean])
+  case class GroupBean(group: String, time: Double, value: Double, nested: Array[NestedBean])
 
-  case class NestedBean(col1: Long, col2: Long)
+  case class NestedBean(col1: Double, col2: Double)
 }
