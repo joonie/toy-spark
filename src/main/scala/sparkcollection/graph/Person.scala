34deb20fda6181303c7ff7824a53b70b82789b22
youngjun.son <syam2000@naver.com>
2020-03-07 16:47:07 +0900

    spark-graphX

---
+++ src/main/scala/sparkcollection/graph/Person.scala
@@ -0,0 +1,19 @@
+package sparkcollection.graph
+
+/**
+ * @author 손영준 (youngjun.son@navercorp.com)
+ */
+case class Person(name: String) {
+  val friends = scala.collection.mutable.ArrayBuffer[Person]()
+
+  def numberOfFriends() = friends.length
+
+  def isFriend(other: Person) = friends.find(_.name == other.name)
+
+  def isConnectedWithin2Steps(other: Person) = {
+    for {f <- friends } yield {
+      f.name == other.name || f.isFriend(other).isDefined
+    }
+  }.find(_ == true).isDefined
+
+}
---
+++ src/main/scala/sparkcollection/graph/personMain.scala
@@ -0,0 +1,33 @@
+package sparkcollection.graph
+
+/**
+ * @author 손영준 (youngjun.son@navercorp.com)
+ */
+object personMain {
+  def main(args: Array[String]) = {
+    val john = Person("Jone")
+    val ken = Person("Ken")
+    val mary = Person("Marry")
+    val dan = Person("Dan")
+
+    john.numberOfFriends()
+
+    john.friends += ken
+    john.numberOfFriends()
+
+    ken.friends += mary
+    ken.numberOfFriends()
+
+    mary.friends += dan
+    mary.numberOfFriends()
+
+    john.isFriend(ken)
+    john.isFriend(mary)
+    john.isFriend(dan)
+
+    john.isConnectedWithin2Steps(ken)
+    john.isConnectedWithin2Steps(mary)
+    john.isConnectedWithin2Steps(dan)
+  }
+
+}
