73fc45a59833a2dfe1f5efe16b51911a3b2d7093
youngjun.son <syam2000@naver.com>
2020-05-13 15:29:04 +0900

    2 files string diff

--- src/main/scala/sparkcollection/file_diff/FileDiffMain.scala
+++ src/main/scala/sparkcollection/file_diff/FileDiffMain.scala
@@ -3,6 +3,9 @@ package sparkcollection.file_diff
 import java.time.{LocalDate, LocalDateTime}
 import java.time.format.DateTimeFormatter
 
+import org.apache.commons.cli.{CommandLine, HelpFormatter, Options, PosixParser}
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.util.GenericOptionsParser
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -18,7 +21,7 @@ object FileDiffMain {
       .config(conf)
       .getOrCreate()
 
-    val currentDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHH"))
+    /*val currentDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHH"))
     println(currentDate)
 
     val beforeDate = LocalDateTime.now().minusDays(1).format(DateTimeFormatter.ofPattern("yyyyMMddHH"))
@@ -30,6 +33,53 @@ object FileDiffMain {
     val diff = after.except(before)
     println(diff.count)
 
-    diff.write.csv("/user/irteamsu/input/ad/diff/"+currentDate)
+    diff.write.csv("/user/irteamsu/input/ad/diff/"+currentDate)*/
+
+    val currentDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHH"))
+    println(currentDate)
+
+    val beforeDate = LocalDateTime.now().minusDays(1).format(DateTimeFormatter.ofPattern("yyyyMMddHH"))
+    println(beforeDate)
+
+    val before = spark.read.csv(conf.get("srcOne"))
+    val after = spark.read.csv(conf.get("srcTwo"))
+
+    val diff = after.except(before)
+    println(diff.count)
+
+    diff.write.csv(conf.get("dst")+currentDate)
+  }
+
+  def getConf(args: Array[String]) = {
+    val conf = new Configuration
+    val otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs()
+
+    val cmd = parseArgs(otherArgs)
+    val srcOne = cmd.getOptionValue("srcOne")
+    val srcTwo = cmd.getOptionValue("srcTwo")
+    val dst = cmd.getOptionValue("dst")
+
+    conf.set("srcOne", srcOne)
+    conf.set("srcTwo", srcTwo)
+    conf.set("dst", dst)
+    conf
+  }
+
+  def parseArgs(args: Array[String]): CommandLine = {
+    val options = new Options()
+
+    var cmd: CommandLine = null
+    try {
+      val parser = new PosixParser()
+      cmd = parser.parse(options, args)
+    } catch {
+      case e: Throwable => {
+        println("ERROR: " + e.getMessage())
+        val formatter = new HelpFormatter()
+        formatter.printHelp("Application ", options, true)
+        sys.exit(-1)
+      }
+    }
+    cmd
   }
 }
