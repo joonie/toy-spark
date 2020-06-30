d63a658f011a1830ce68999708a2ebee25e9d902
youngjun.son <syam2000@naver.com>
2020-06-30 23:59:34 +0900

    csv read sample1

---
+++ src/main/scala/sparkcollection/fileread/csv/Sample1Main.scala
@@ -0,0 +1,22 @@
+package sparkcollection.fileread.csv
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * @author 손영준 (youngjun.son@navercorp.com)
+ */
+object AccumulatorMain {
+  def main(args: Array[String]) : Unit = {
+    val conf = new SparkConf().setAppName("sample1")
+    val spark = SparkSession
+      .builder()
+      .appName("fileDiffMain")
+      .config(conf)
+      .getOrCreate()
+
+    val flightData2015 = spark.read.option("inferSchema", "true").option("header", "true").csv("/user/irteamsu/input/2015-summary.csv")
+
+    flightData2015.take(3)
+  }
+}
